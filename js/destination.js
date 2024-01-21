2016c9b8efc74b86feefeb3dcf4aae44b366b1fc
kaliche05 <157325137+kaliche05@users.noreply.github.com>
2024-01-21 18:00:17 -0500

    First commit

---
+++ js/destination.js
@@ -0,0 +1,24 @@
+function irAEmpresa() {
+    // Utiliza la función scrollIntoView para desplazarte a la sección con id "seccionDestino"
+    document.getElementById('seccionEmpresa').scrollIntoView({ behavior: 'smooth' });
+}
+
+function irAMantenimiento() {
+    // Utiliza la función scrollIntoView para desplazarte a la sección con id "seccionDestino"
+    document.getElementById('seccionMantenimiento').scrollIntoView({ behavior: 'smooth' });
+}
+
+function irAPreguntas() {
+    // Utiliza la función scrollIntoView para desplazarte a la sección con id "seccionDestino"
+    document.getElementById('seccionPreguntas').scrollIntoView({ behavior: 'smooth' });
+}
+
+function irAContacto() {
+    // Utiliza la función scrollIntoView para desplazarte a la sección con id "seccionDestino"
+    document.getElementById('seccionContacto').scrollIntoView({ behavior: 'smooth' });
+}
+
+function irAInicio() {
+    // Utiliza la función scrollIntoView para desplazarte a la sección con id "seccionDestino"
+    document.getElementById('seccionInicio').scrollIntoView({ behavior: 'smooth' });
+}
